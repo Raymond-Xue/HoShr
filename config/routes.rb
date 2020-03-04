79e3fa0725beb8c12c49af8c3da42e7e7cb263ea
hantingge <hanting.ge@outlook.com>
2020-03-03 20:29:58 -0500

    Signup and Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :groups
   root 'static_pages#home'
   get 'static_pages/home'
-  
+  get  '/signup',  to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to:'sessions#destroy'
+
   resources :cities
   resources :states
   resources :countries
@@ -10,10 +15,7 @@ Rails.application.routes.draw do
   resources :types
   resources :rooms
   resources :lessee_requests
-  resources :lessor_requests
-  
-  
- 
+  resources :lessor_requests 
   resources :properties
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_204903) do
+ActiveRecord::Schema.define(version: 2020_02_28_213050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -140,7 +140,6 @@ ActiveRecord::Schema.define(version: 2020_02_28_204903) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
     t.string "firstname"
     t.string "middlename"
     t.string "lastname"
@@ -150,6 +149,7 @@ ActiveRecord::Schema.define(version: 2020_02_28_204903) do
     t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "cities", "states"
