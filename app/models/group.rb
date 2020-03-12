fe03989060ee98dc53095910c1af8a1cd4f0a2ae
hantingge <47905519+hantingge@users.noreply.github.com>
2020-03-12 15:21:02 -0400

    Revert "Recommend (#1)"
    This reverts commit 4ff81579f0182541c83431a039ffb2107f0ed654.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,3 @@
 class Group < ApplicationRecord
-	has_many :lessee_requests
-	has_many :users
-
-	has_many :send_invitation, foreign_key: "group_from_id", class_name: "Invitation"
-	has_many :received_invitation, foreign_key: "group_to_id", class_name: "Invitation"
+	has_many :lessee_request
 end
--- test/controllers/recommend_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RecommendControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
